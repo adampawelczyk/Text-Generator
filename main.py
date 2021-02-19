f515e60d5477f10b2a5838865411fa0b0f781ef5
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-02-19 23:12:15 +0100

    Create a Markov chain model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from nltk.tokenize import WhitespaceTokenizer
 from nltk import bigrams
+from collections import defaultdict, Counter
 
 
 file_name = input()
@@ -8,22 +9,27 @@ with open(file_name, encoding='utf-8') as file:
     file_content = file.read()
     whitespace_tokenizer = WhitespaceTokenizer()
     file_tokens = whitespace_tokenizer.tokenize(file_content)
-    bigram = list(bigrams(file_tokens))
+    bigrams = list(bigrams(file_tokens))
+    bigrams_dict = defaultdict(list)
+    markov_model = {}
 
-    print(f"Number of bigrams: {len(bigram)}")
-    print()
+    for bigram in bigrams:
+        bigrams_dict[bigram[0]].append(bigram[1])
+
+    for key in bigrams_dict.keys():
+        markov_model[key] = Counter(bigrams_dict[key]).most_common()
 
     while True:
-        try:
-            index = input()
-            if index == "exit":
-                exit(0)
-            index = int(index)
+        index = input()
+        if index == "exit":
+            exit(0)
 
-            if index > len(bigram) - 1:
-                print("Index error. Please input an integer that is not greater that the number of all bigrams.")
-            else:
-                print(f"Head: {bigram[index][0]}\t\tTail: {bigram[index][1]}")
+        print(f"Head: {index}")
 
-        except ValueError:
-            print("Type Error. Please input an integer.")
+        if index in markov_model:
+            for item in markov_model[index]:
+                print(f"Head: {item[0]}\t\tTail: {item[1]}")
+        else:
+            print("Key Error. The requested word is not in the model. Please input another one")
+
+        print()
