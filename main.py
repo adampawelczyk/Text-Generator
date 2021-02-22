b7e59e4c152bb485cdf3bda11644014e5b74ff56
Adam Pawelczyk <pawelczyk.adam@hotmail.com>
2021-02-22 21:59:24 +0100

    Extend the program to create a Markov model based on trigrams in order to generate more sensible sentences

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from nltk.tokenize import WhitespaceTokenizer
-from nltk import bigrams
+from nltk import trigrams
 from collections import defaultdict, Counter
 from random import choices, choice
 
@@ -16,10 +16,11 @@ def generate_sentence():
 
     while True:
         if len(sentence) == 0:
-            word = choice(list(markov_model.keys()))
+            word = choice(list(markov_model.keys())).split(" ")
             while word[0].islower() or not word[0].isalpha():
-                word = choice(list(markov_model.keys()))
-            sentence.append(word)
+                word = choice(list(markov_model.keys())).split(" ")
+            sentence.append(word[0])
+            sentence.append(word[1])
         else:
             if sentence[-1][-1] in ".,?":
                 if len(sentence) < 5:
@@ -28,26 +29,47 @@ def generate_sentence():
                 else:
                     return sentence
             else:
-                next_word = predict_next_word(word)
+                next_word = predict_next_word(" ".join(sentence[-2:]))
                 word = next_word
                 sentence.append(word)
 
 
-file_name = input()
-
-with open(file_name, encoding='utf-8') as file:
-    file_content = file.read()
+def tokenize_text(text):
     whitespace_tokenizer = WhitespaceTokenizer()
-    file_tokens = whitespace_tokenizer.tokenize(file_content)
-    bigrams = list(bigrams(file_tokens))
-    bigrams_dict = defaultdict(list)
+
+    return whitespace_tokenizer.tokenize(text)
+
+
+def create_markov_model(text_tokens):
+    trigrams_list = list(trigrams(text_tokens))
+    trigrams_dict = defaultdict(list)
     markov_model = {}
 
-    for bigram in bigrams:
-        bigrams_dict[bigram[0]].append(bigram[1])
+    for trigram in trigrams_list:
+        trigrams_dict[trigram[0] + ' ' + trigram[1]].append(trigram[2])
 
-    for key in bigrams_dict.keys():
-        markov_model[key] = Counter(bigrams_dict[key]).most_common()
+    for key in trigrams_dict.keys():
+        markov_model[key] = Counter(trigrams_dict[key]).most_common()
 
+    return markov_model
+
+
+def read_file(file_name):
+    with open(file_name, encoding='utf-8') as file:
+        file_content = file.read()
+
+        return file_content
+
+
+def print_sentences():
     for _ in range(10):
         print(*generate_sentence())
+
+
+file_name = input()
+
+file_content = read_file(file_name)
+file_tokens = tokenize_text(file_content)
+markov_model = create_markov_model(file_tokens)
+
+print_sentences()
